890c8d1e076c8d791945f86c10858d9435eb5548
Himanshu <himanshu.mishra@vinsol.com>
2017-11-28 12:16:49 +0530

    done

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,9 +14,7 @@ class User < ApplicationRecord
   private
 
     def ensure_an_admin_remains
-      if User.count.zero?
-        raise Error.new "Can't delete last user"
-      end
+      raise Error.new "Can't delete last user" if User.count.zero?
     end
 
     def ensure_user_is_not_admin
