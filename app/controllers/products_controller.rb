a98a4b7297e420c6644e30e4d12b2907442bb2bf
Himanshu <himanshu.mishra@vinsol.com>
2017-12-14 11:27:10 +0530

    Changes as discussed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -46,7 +46,6 @@ class ProductsController < ApplicationController
   # PATCH/PUT /products/1
   # PATCH/PUT /products/1.json
   def update
-
     respond_to do |format|
       if @product.update(product_params)
         format.html { redirect_to @product, notice: 'Product was successfully updated.' }
